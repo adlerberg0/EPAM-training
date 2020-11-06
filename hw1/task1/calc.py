6f43b50bf59ec1b33e9884bec86f04cc2df23311
adlerberg0 <artur-korotkij@yandex.ru>
2020-11-07 01:40:39 +0300

    rename sample_project to hw1

--- hw1/task1/calc.py
+++ hw1/task1/calc.py
@@ -3,8 +3,8 @@ please create your own git repository on github.
 setup pre-commit hook with black and isort formatting for the repo
 initialize .gitignore in the repository root (you can use this sample)
 create a homework1 directory in the repo
-then copy the sample_project into the directory.
-fix all bugs in the sample_project code
+then copy the hw1 into the directory.
+fix all bugs in the hw1 code
 write an extra test for each found bug
 Note: as we said, any hw, which does not pass
  isort --profile black --check and black --check, will be rejected
