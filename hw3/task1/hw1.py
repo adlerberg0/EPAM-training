04134e7bd082797227c08145e97199bc26f9bba3
adlerberg0 <artur-korotkij@yandex.ru>
2020-11-08 19:55:22 +0300

    add hw3

---
+++ hw3/task1/hw1.py
@@ -0,0 +1,24 @@
+from typing import Any, Callable, Sequence
+
+
+def cache(times: int) -> Callable:
+    # use memoization technique
+    def cache_dec(func: Callable):
+        memoization = {}
+
+        def wrapper(*args: Sequence[Any]) -> Any:
+            if args in memoization and memoization[args][1] > 0:
+                memoization[args][1] -= 1
+                return memoization[args][0]
+            else:
+                memoization[args] = [func(*args), times]
+                return memoization[args][0]
+
+        return wrapper
+
+    return cache_dec
+
+
+@cache(times=3)
+def f():
+    return input("? ")  # careful with input() in python2, use raw_input() instead
---
+++ hw3/task2/hw2.py
@@ -0,0 +1,43 @@
+"""
+Here's a not very efficient calculation function that calculates something important::
+
+    import time
+    import struct
+    import random
+    import hashlib
+
+    def slow_calculate(value):
+        #Some weird voodoo magic calculations
+        time.sleep(random.randint(1,3))
+        data = hashlib.md5(str(value).encode()).digest()
+        return sum(struct.unpack('<' + 'B' * len(data), data))
+
+Calculate total sum of slow_calculate() of all numbers starting from 0 to 500.
+Calculation time should not take more than a minute. Use functional capabilities of multiprocessing module.
+You are not allowed to modify slow_calculate function.
+
+"""
+import hashlib
+import random
+import struct
+from multiprocessing import Pool
+from time import perf_counter, sleep
+
+
+def slow_calculate(value):
+    """Some weird voodoo magic calculations"""
+    sleep(random.randint(1, 3))
+    data = hashlib.md5(str(value).encode()).digest()
+    return sum(struct.unpack("<" + "B" * len(data), data))
+
+
+def call_func(data, func=slow_calculate):
+    p = Pool(25)
+    return p.map(func, data)
+
+
+if __name__ == "__main__":
+    t1 = perf_counter()
+    call_func(list(range(0, 501)))
+    t2 = perf_counter()
+    print("Elapsed time during the whole program in seconds:", t2 - t1)
